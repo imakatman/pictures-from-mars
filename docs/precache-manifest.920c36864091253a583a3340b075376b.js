e462fe65cd0c555c76500889a91edb76df39af14
Hope Kim <hopekim511@protonmail.com>
2019-09-29 19:31:11 -0700

    Renamed build to docs

---
+++ docs/precache-manifest.920c36864091253a583a3340b075376b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de4bdc40f3e09298e126ee3fa411744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8801b376ca5273ae9dcf",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "20407bbcdbde4a343842",
+    "url": "/static/js/2.f71dbe52.chunk.js"
+  },
+  {
+    "revision": "8801b376ca5273ae9dcf",
+    "url": "/static/js/main.d9a10634.chunk.js"
+  },
+  {
+    "revision": "de7d1e38148f710a13ed",
+    "url": "/static/js/runtime-main.3e7bd236.js"
+  }
+]);
