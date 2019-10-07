a75987a00332ae2b23bcbe0680d622a3c213d900
Hope Kim <hopekim511@protonmail.com>
2019-10-06 21:37:19 -0700

    Trying to move build to root

---
+++ precache-manifest.16611691e053e3cda4ef8c0914e3212f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5bd39302b698fe16ecac82dd2c5b5f",
+    "url": "/pictures-from-mars-v3/index.html"
+  },
+  {
+    "revision": "e77fd36bbd7401c0a0a8",
+    "url": "/pictures-from-mars-v3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "20407bbcdbde4a343842",
+    "url": "/pictures-from-mars-v3/static/js/2.f71dbe52.chunk.js"
+  },
+  {
+    "revision": "e77fd36bbd7401c0a0a8",
+    "url": "/pictures-from-mars-v3/static/js/main.15967c0b.chunk.js"
+  },
+  {
+    "revision": "55e4c7131fe62b2c82ed",
+    "url": "/pictures-from-mars-v3/static/js/runtime-main.d98a062c.js"
+  }
+]);
