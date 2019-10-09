15373512301a5122c5f881f4d46813fcf2abcac4
Hope Kim <hopekim511@protonmail.com>
2019-10-09 12:38:15 -0700

    Created a new build with second-draft stuff

---
+++ precache-manifest.28ef8e383158cb3dfe86fa78003a4bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217b6470c255d115fc9d797d42ecf46c",
+    "url": "/pictures-from-mars-v3/index.html"
+  },
+  {
+    "revision": "84fba819e9d3b402fe98",
+    "url": "/pictures-from-mars-v3/static/css/main.8f8f11be.chunk.css"
+  },
+  {
+    "revision": "b7e2ddd810c88e60e663",
+    "url": "/pictures-from-mars-v3/static/js/2.4289a41e.chunk.js"
+  },
+  {
+    "revision": "84fba819e9d3b402fe98",
+    "url": "/pictures-from-mars-v3/static/js/main.0d2f286f.chunk.js"
+  },
+  {
+    "revision": "55e4c7131fe62b2c82ed",
+    "url": "/pictures-from-mars-v3/static/js/runtime-main.d98a062c.js"
+  }
+]);
